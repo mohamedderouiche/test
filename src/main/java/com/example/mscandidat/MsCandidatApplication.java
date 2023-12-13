b1685badd0aa7991a424d113c3283e461688ab8a
Ikram Hannechi <ikram.hannechi@esprit.tn>
2023-12-13 00:54:22 +0000

    hey

--- src/main/java/com/example/mscandidat/MsCandidatApplication.java
+++ src/main/java/com/example/mscandidat/MsCandidatApplication.java
@@ -1,5 +1,5 @@
 package com.example.mscandidat;//////////////////////////////////
-//////////////:mmmmmmmmm
+//////////////:mmmmmmmmmMMM@
 import com.example.mscandidat.Entity.Candidat;
 import com.example.mscandidat.Repository.CandidatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
